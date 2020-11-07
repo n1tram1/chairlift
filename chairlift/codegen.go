4f73aba0f37d9570650f35875231c437d52d25a4
Martin Schmidt <martin.schmidt@epita.fr>
2020-11-07 16:53:20 +0100

    codegen: generate LdVxByte

--- chairlift/codegen.go
+++ chairlift/codegen.go
@@ -1,5 +1,9 @@
 package chairlift
 
+import (
+    "errors"
+)
+
 func compile_instructions(instructions []Instruction, c *Compiler) error {
     for _, inst := range instructions {
         err := inst.compile(c)
@@ -12,137 +16,142 @@ func compile_instructions(instructions []Instruction, c *Compiler) error {
 }
 
 func (cls *Cls) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ret  *Ret) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (jp *JpAddr) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (call *CallAddr) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (se *SeVxByte) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (sne *SneVxByte) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (se *SeVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdVxByte) compile(c *Compiler) error {
-    panic("unimplemented")
+    dst := c.VRegToLLVMValue(ld.vx)
+    imm := c.ConstUint8(ld.kk)
+
+    c.builder.CreateStore(imm, dst)
+
+    return nil
 }
 
 func (add *AddVxByte) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (or *OrVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (and *AndVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (xor *XorVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (add *AddVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (sub *SubVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (shr *ShrVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (subn *SubnVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (shl *ShlVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (sne *SneVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdIAddr) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (jp *JpV0Addr) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (rnd *Rnd) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (drw *DrwVxVy) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (skp *SkpVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (sknp *SknpVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdVxDt) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdVxK) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdDtVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdStVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (add *AddIVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdFVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdBVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdIVx) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
 
 func (ld *LdVxI) compile(c *Compiler) error {
-    panic("unimplemented")
+    return errors.New("unimplemented instruction in codegen")
 }
--- chairlift/compiler.go
+++ chairlift/compiler.go
@@ -4,6 +4,7 @@ import (
     // "llvm.org/llvm/bindings/go/llvm"
     llvm "github.com/tinygo-org/go-llvm"
     "os"
+    "fmt"
 )
 
 type Compiler struct {
@@ -14,8 +15,65 @@ type Compiler struct {
 
     mainFn llvm.Value
 
-    v0 llvm.Value
-    v1 llvm.Value
+    reg_v0 llvm.Value
+    reg_v1 llvm.Value
+    reg_v2 llvm.Value
+    reg_v3 llvm.Value
+    reg_v4 llvm.Value
+    reg_v5 llvm.Value
+    reg_v6 llvm.Value
+    reg_v7 llvm.Value
+    reg_v8 llvm.Value
+    reg_v9 llvm.Value
+    reg_vA llvm.Value
+    reg_vB llvm.Value
+    reg_vC llvm.Value
+    reg_vD llvm.Value
+    reg_vE llvm.Value
+    reg_vF llvm.Value
+}
+
+func (c *Compiler) VRegToLLVMValue(vreg VReg) llvm.Value {
+    switch vreg {
+        case V0:
+            return c.reg_v0
+        case V1:
+            return c.reg_v1
+        case V2:
+            return c.reg_v2
+        case V3:
+            return c.reg_v3
+        case V4:
+            return c.reg_v4
+        case V5:
+            return c.reg_v5
+        case V6:
+            return c.reg_v6
+        case V7:
+            return c.reg_v7
+        case V8:
+            return c.reg_v8
+        case V9:
+            return c.reg_v9
+        case VA:
+            return c.reg_vA
+        case VB:
+            return c.reg_vB
+        case VC:
+            return c.reg_vC
+        case VD:
+            return c.reg_vD
+        case VE:
+            return c.reg_vE
+        case VF:
+            return c.reg_vF
+        default:
+            panic(fmt.Sprintf("unkown register %X", vreg))
+    }
+}
+
+func (c *Compiler) ConstUint8(x uint8) llvm.Value {
+    return llvm.ConstInt(llvm.Int8Type(), uint64(x), false)
 }
 
 func (c *Compiler) selectBlock(bb llvm.BasicBlock) {
@@ -45,23 +103,41 @@ func (c *Compiler) createByteRegister(name string) llvm.Value {
 }
 
 func (c *Compiler) createRegisters() {
-    c.v0 = c.createByteRegister("V0")
-    c.v1 = c.createByteRegister("V1")
+    c.reg_v0 = c.createByteRegister("V0")
+    c.reg_v1 = c.createByteRegister("V1")
+    c.reg_v2 = c.createByteRegister("V2")
+    c.reg_v3 = c.createByteRegister("V3")
+    c.reg_v4 = c.createByteRegister("V4")
+    c.reg_v5 = c.createByteRegister("V5")
+    c.reg_v6 = c.createByteRegister("V6")
+    c.reg_v7 = c.createByteRegister("V7")
+    c.reg_v8 = c.createByteRegister("V8")
+    c.reg_v9 = c.createByteRegister("V9")
+    c.reg_vA = c.createByteRegister("VA")
+    c.reg_vB = c.createByteRegister("VB")
+    c.reg_vC = c.createByteRegister("VC")
+    c.reg_vD = c.createByteRegister("VD")
+    c.reg_vE = c.createByteRegister("VE")
+    c.reg_vF = c.createByteRegister("VF")
 }
 
-func (c *Compiler) compile(rom *Rom) error {
-    c.createRegisters()
-
+func (c *Compiler) createMain() {
     mainType := llvm.FunctionType(llvm.Int32Type(), []llvm.Type{llvm.PointerType(llvm.Int8Type(), 0), llvm.PointerType(llvm.Int8Type(), 0)}, false)
 
     c.mainFn = llvm.AddFunction(c.mod, "main", mainType)
     c.mainFn.SetFunctionCallConv(llvm.CCallConv)
 
     entry := llvm.AddBasicBlock(c.mainFn, "entry")
-    c.builder.SetInsertPointAtEnd(entry)
+    c.selectBlock(entry)
+}
+
+func (c *Compiler) compile(rom *Rom) error {
+    c.createRegisters()
+    c.createMain()
 
     err := compile_instructions(rom.instructions, c)
     if err != nil {
+        c.mod.Dump()
         return err
     }
 
