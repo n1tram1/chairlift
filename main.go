0039547774ff35ab8c2a72c286afdab871082935
Martin Schmidt <martin.schmidt@epita.fr>
2020-11-17 23:10:42 +0100

    main: cleanup

--- main.go
+++ main.go
@@ -1,33 +1,32 @@
 package main
 
 import (
-    "chairlift/chairlift"
-    "fmt"
-    "os"
-    "log"
-    "strings"
-    "path"
+	"chairlift/chairlift"
+	"fmt"
+	"log"
+	"os"
+	"path"
+	"strings"
 )
 
 func main() {
-    for _, arg := range os.Args[1:] {
-        fmt.Println("processing ", arg)
+	for _, arg := range os.Args[1:] {
+		fmt.Println("processing ", arg)
 
-        rom, err := chairlift.OpenRom(arg)
-        if err != nil {
-            log.Fatal(err)
-            continue
-        }
-        fmt.Println("rom: ", rom)
+		rom, err := chairlift.OpenRom(arg)
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
 
-        name := strings.TrimSuffix(arg, path.Ext(arg))
+		name := strings.TrimSuffix(arg, path.Ext(arg))
 
-        _, err = chairlift.CompileRomToFile(rom, name + ".bc")
-        if err != nil {
-            log.Fatal(err)
-            continue
-        }
+		_, err = chairlift.CompileRomToFile(rom, name+".bc")
+		if err != nil {
+			log.Fatal(err)
+			continue
+		}
 
-        fmt.Println()
-    }
+		fmt.Println()
+	}
 }
